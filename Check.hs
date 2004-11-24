091d23d538a981a7edbb2901ddf9e9df4b19dfe2
Masahiro Sakai <masahiro.sakai@gmail.com>
2004-11-24 12:14:26 +0000

    git-svn-id: svn+ssh://kencho.tom.sfc.keio.ac.jp/home/sakai/svn/hs-hyperset/trunk@45 69ec182e-48e9-0310-9dd4-fc2a491b59fc

--- Check.hs
+++ Check.hs
@@ -1,14 +1,27 @@
 import Debug.QuickCheck
 import Hyperset
 
-prop_unionSize (x,y) =
+prop_cardinalityNonNegative (x :: Set Int) = cardinality x >= 0
+prop_cardinality1 (x :: Set Int) = cardinality x == length (toList x)
+
+{-
+prop_propersubsetIsSubset (x, y :: Set Int) =
+    x `properSubsetOf` y ==> x `subsetOf` y
+-}
+
+prop_toList (x :: Set Int) = fromList (toList x) == x
+
+prop_unionSize (x, y :: Set Int) =
     cardinality (x `union` y) <= cardinality x + cardinality y
-    where hoge = x :: Set Int
+prop_unionInclusion1 (x, y :: Set Int) = x `subsetOf` (x `union` y)
+prop_unionInclusion2 (x,y :: Set Int)  = y `subsetOf` (x `union` y)
 
-prop_unionInclusion1 (x,y) = x `subsetOf` (x `union` y)
-    where hoge = x :: Set Int
+-- 時間がかかり過ぎないようにサイズを予め制限しておく
+prop_powersetSize (x :: Set Int) =
+    c <= 10 ==> 2^c == cardinality (powerset x)
+    where c = cardinality x
 
-prop_unionInclusion2 (x,y) = y `subsetOf` (x `union` y)
-    where hoge = x :: Set Int
-
-main = quickCheck prop_unionInclusion1
+prop_powerset (x :: Set Int) =
+   (cardinality x) <= 10 ==>
+        (Right x) `member` px && (Right emptySet) `member` px
+    where px = powerset x
